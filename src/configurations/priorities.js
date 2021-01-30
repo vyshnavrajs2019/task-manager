95b32ed398333ac163f0c2020beded5a22cbab58
vyshnav.sp <vyshnav.sp@zohocorp.com>
2021-01-30 11:53:51 +0530

    [IMPROVEMENT] priorities: Assign value and name

--- src/configurations/priorities.js
+++ src/configurations/priorities.js
@@ -1,5 +1,5 @@
-export const HIGH_PRIORITY = 'HIGH';
-export const MEDIUM_PRIORITY = 'MEDIUM';
-export const LOW_PRIORITY = 'LOW';
+export const HIGH_PRIORITY = { name: 'HIGH', value: 3 };
+export const MEDIUM_PRIORITY = { name: 'MEDIUM', value: 2 };
+export const LOW_PRIORITY = { name: 'LOW', value: 1 };
 
 export const priorities= [HIGH_PRIORITY, MEDIUM_PRIORITY, LOW_PRIORITY];
