199f763d240860780e104f3505708f4be69d4da8
vyshnav.sp <vyshnav.sp@zohocorp.com>
2021-01-30 09:54:45 +0530

    [IMPROVEMENT] App: Priority configuration

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,15 @@ import Tasks from '../components/Tasks';
 
 import classes from './App.module.scss';
 
+const HIGH_PRIORITY = 'HIGH';
+const MEDIUM_PRIORITY = 'MEDIUM';
+const LOW_PRIORITY = 'LOW';
+
+const priorities= [HIGH_PRIORITY, MEDIUM_PRIORITY, LOW_PRIORITY];
+
 function App() {
+	const tasks = priorities.map((priority, index) => <Tasks key={index} priority={priority} />);
+
 	return (
 		<div className={classes.App}>
 			<div className={classes.AppCenter}>
@@ -15,9 +23,7 @@ function App() {
 					<button type="button" className={classes.App__MenuButton}>Create Task</button>
 				</div>
 				<div className={classes.App__TasksContainer}>
-					<Tasks />
-					<Tasks />
-					<Tasks />
+					{tasks}
 				</div>
 			</div>
 		</div>
