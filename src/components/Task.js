c349d410fa5c2c66dce47ab4c5dd7abfa0c58ce7
vyshnav.sp <vyshnav.sp@zohocorp.com>
2021-01-30 15:23:16 +0530

    [IMPROVEMENT] Task: Add created on and no task owner info

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,15 +3,15 @@ import React from 'react';
 import classes from './Task.module.scss';
 
 function Task({ task, owners }) {
-	let taskOwner = null;
+	let taskOwner = <span style={{fontSize: 12, color: 'grey'}}>No task owner</span>;
 	if (task.assigned_to) {
 		const owner = owners.find(owner => owner.id === task.assigned_to);
 		taskOwner = (
-			<div className={classes.Task__Owner}>
+			<React.Fragment>
 				<div className={classes.Task__OwnerPicture} style={{ backgroundImage: `url(${owner?.picture || ''})` }}>
 				</div>
 				<span>{task.assigned_name}</span>
-			</div>
+			</React.Fragment>
 		)
 	}
 
@@ -20,9 +20,14 @@ function Task({ task, owners }) {
 			<div className={classes.Task__Title}>
 				<span>{task.message}</span>
 			</div>
-			{taskOwner}
+			<div className={classes.Task__Owner}>
+				{taskOwner}
+			</div>
 			<div className={classes.Task__Date}>
-				<span>{task.due_date}</span>
+				<span>Due: {task.due_date}</span>
+			</div>
+			<div className={classes.Task__Date}>
+				<span>Created: {task.created_on}</span>
 			</div>
 		</div>
 	)
