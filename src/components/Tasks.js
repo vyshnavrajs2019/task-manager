a848359493a446118e3f5d354e57f0956767d055
vyshnav.sp <vyshnav.sp@zohocorp.com>
2021-01-30 14:58:25 +0530

    [IMPROVEMENT] Tasks: Load users

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import Task from './Task';
+import React, { useState, useEffect } from 'react';
 
+import { getUsers } from '../configurations/api';
+import Task from './Task';
 import classes from './Tasks.module.scss';
 
-function Tasks() {
+function Tasks({ tasks, priority }) {
+	const [owners, setOwners] = useState([]);
+	
+	useEffect(() => {
+		getUsers()
+			.then(response => response.json())
+			.then(responseData => setOwners(responseData.users))
+			.catch(err => { alert('ERROR OCCURED'); console.log(err); })
+	}, []);
+
+	const taskList = tasks
+						.filter(task => +task.priority === priority.value)
+						.map(task => <Task key={task.taskid} task={task} owners={owners} />)
 	return (
 		<div className={classes.Tasks}>
-			<Task />
-			<Task />
-			<Task />
-			<Task />
+			<h3>{priority.name}</h3>
+			{taskList}
 		</div>
 	)
 }
