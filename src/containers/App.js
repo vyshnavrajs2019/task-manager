de9eb07b3065e3fe544620e9387871f97f98553b
vyshnav.sp <vyshnav.sp@zohocorp.com>
2021-01-30 09:14:14 +0530

    [IMPROVEMENT] App: Add menu-handler and search

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,13 @@ function App() {
 	return (
 		<div className={classes.App}>
 			<div className={classes.AppCenter}>
-				
+				<h1>Task Manager</h1>
+				<div className={classes.App__Search}>
+					<input className={classes.App__SearchInput} type="text" value="" placeholder="Search..." />
+				</div>
+				<div className={classes.App__MenuHandler}>
+					<button type="button" className={classes.App__MenuButton}>Create Task</button>
+				</div>
 			</div>
 		</div>
 	);
